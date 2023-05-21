6001d521d0a4cf50a6217cf296a081e909d2f9d3
Dron Hazra <dronh.to@gmail.com>
2023-05-21 21:26:25 +0100

    refactored calendar operations out of main

--- lib/fetch_ical.dart
+++ lib/fetch_ical.dart
@@ -1,13 +1,15 @@
 import 'package:http/http.dart' as http;
 
 class fetchIcal {
-  static Future<String> fetch(link) async {
+  static Future<String> fetch(String link) async {
+    print(link);
     var response = await http.get(Uri.parse(link));
     response.statusCode == 200
         ? print("Success")
         : print("Error: ${response.statusCode}");
+    if (response.statusCode != 200) {
+      throw Exception("Failed to access ical");
+    }
     return response.body;
   }
-
 }
-
